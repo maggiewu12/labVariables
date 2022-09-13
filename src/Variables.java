9f9043f84056104d51c788a510a0f2280db4f633
Maggie Wu <100169470@lubbockisd.net>
2022-09-13 08:14:51 -0500

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,37 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte byteOne = 127;
+        short sss = 4865;
+        int iii = 222222;
+        long lll = 3333333;
+
+        float fff = 4.4f;
+        double ddd = 7.777;
+
+        boolean bbb = false;
+
+        char ccc = 'C';
+
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Maggie Wu            09/12/22*");
+        System.out.println("*        integer types          *");
+        System.out.println(sss);
+        System.out.println(iii);
+        System.out.println(lll);
+        System.out.println(fff);
+        System.out.println(ddd);
+        System.out.println(bbb);
+        System.out.println(ccc);
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+    }
+}
